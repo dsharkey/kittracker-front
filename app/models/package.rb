0182d0b3142877c33df06966b62e2501e642085a
Stuart Hinson <stuart@ecoscorecard.com>
2011-06-04 11:35:43 -0400

    assoc with status

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,5 +1,10 @@
 class Package < ActiveRecord::Base
   belongs_to :project
+  has_many :statuses
   
   Package::STATUSES = {"In Transit" => 0,"Delivered" => 1,"Delivered -- Damaged" => 2}
+  
+  def last_status
+    Status.find(:order => "created_at desc",:limit => 1)
+  end
 end
